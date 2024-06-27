694ebebcde78cce40c83b8c596deff15b3bfb936
duycs <duycs@outlook.com>
2024-06-27 17:41:57 +0700

    add rsa

---
+++ utils/rsa.ts
@@ -0,0 +1,82 @@
+import * as crypto from "crypto";
+
+const generateKeys = (length: number) => {
+    const data = crypto.generateKeyPairSync("rsa", {
+        modulusLength: length ?? 2048,
+        publicKeyEncoding: {
+          type: "pkcs1",
+          format: "pem",
+        },
+        privateKeyEncoding: {
+          type: "pkcs1",
+          format: "pem",
+        },
+    });
+
+    return data;
+}
+
+const encryptedData = (data: any, publicKey: string) => crypto.publicEncrypt(
+	{
+		key: publicKey,
+		padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+		oaepHash: "sha256",
+	},
+	// We convert the data string to a buffer using `Buffer.from`
+	Buffer.from(data)
+)
+
+const decryptedData = (encryptedData: any, privateKey: string) =>  crypto.privateDecrypt(
+	{
+		key: privateKey,
+		// In order to decrypt the data, we need to specify the
+		// same hashing function and padding scheme that we used to
+		// encrypt the data in the previous step
+		padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+		oaepHash: "sha256",
+	},
+	encryptedData
+)
+
+const signature = (verifiableData: any, privateKey: string) => crypto.sign("sha256", Buffer.from(verifiableData), {
+	key: privateKey,
+	padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+})
+
+const isVerified = (verifiableData: any, signature: any, publicKey: string) => crypto.verify(
+	"sha256",
+	Buffer.from(verifiableData),
+	{
+		key: publicKey,
+		padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+	},
+	signature
+)
+
+const dataBase64 = (data: any) => {
+    return Buffer.from(data).toString("base64");
+}
+
+export const encrypt  = (data : string, publicKey: string) => {
+    return dataBase64(encryptedData(data, publicKey));
+}
+
+export const decrypt  = (data : string, privateKey: string) => {
+    return decryptedData(data, privateKey).toString();
+}
+
+export const verify  = (data : string, signature : string, publicKey: string) => {
+    return isVerified(data, signature, publicKey);
+}
+
+export const sign  = (data : string, privateKey: string) => {
+    return dataBase64(signature(data, privateKey));
+}
+
+export const toBase64  = (data : string) => {
+    return dataBase64(data);
+}
+
+export const genKeys = (length: number) => {
+    return generateKeys(length);
+}
---
+++ utils/utils.routes.ts
@@ -0,0 +1,86 @@
+import express, { Request, Response } from "express"
+import { StatusCodes } from "http-status-codes"
+import * as rsa from "./rsa"
+
+export const utilRoutes = express.Router()
+
+utilRoutes.post("/rsa/keys", async (req : Request, res : Response) => {
+    try {
+        const { length } = req.body;
+
+        const data = await rsa.genKeys(length);
+
+        if (!data) {
+            return res.status(StatusCodes.BAD_REQUEST).json({message : `Invalid data`})
+        }
+
+        return res.status(StatusCodes.OK).json({data})
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+    }
+})
+
+utilRoutes.post("/rsa/encrypt", async (req : Request, res : Response) => {
+    try {
+        const { payload, publicKey } = req.body;
+
+        const data = await rsa.encrypt(payload, publicKey);
+
+        if (!data) {
+            return res.status(StatusCodes.BAD_REQUEST).json({message : `Invalid data`})
+        }
+
+        return res.status(StatusCodes.OK).json({data})
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+    }
+})
+
+utilRoutes.post("/rsa/decrypt", async (req : Request, res : Response) => {
+    try {
+        const { payload, privateKey } = req.body
+
+        const data = await rsa.decrypt(payload, privateKey);
+
+        if (!data) {
+            return res.status(StatusCodes.BAD_REQUEST).json({message : `Invalid data`})
+        }
+
+        return res.status(StatusCodes.OK).json({data})
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+    }
+})
+
+utilRoutes.post("/rsa/sign", async (req : Request, res : Response) => {
+    try {
+        const { payload, privateKey } = req.body
+
+        const data = await rsa.sign(payload, privateKey);
+
+        if (!data) {
+            return res.status(StatusCodes.BAD_REQUEST).json({message : `Invalid data`})
+        }
+
+        return res.status(StatusCodes.OK).json({data})
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+    }
+})
+
+
+utilRoutes.post("/rsa/verify", async (req : Request, res : Response) => {
+    try {
+        const { payload, signature, publicKey } = req.body
+
+        const success = await rsa.verify(payload, signature, publicKey);
+
+        if (!success) {
+            return res.status(StatusCodes.BAD_REQUEST).json({message : `Invalid data`})
+        }
+
+        return res.status(StatusCodes.OK).json({success})
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+    }
+})
